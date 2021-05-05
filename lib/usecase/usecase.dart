79d8bbd50e2dbb8c2911897956c559725c974ea3
Otoniel Duarte <otonielduarte2@gmail.com>
2021-05-05 09:28:51 -0300

    add no params

--- lib/usecase/usecase.dart
+++ lib/usecase/usecase.dart
@@ -1,6 +1,12 @@
 import 'package:core/helpers/mapper.dart';
 import 'package:core/usecase/errors/failures.dart';
+import 'package:equatable/equatable.dart';
 
 abstract class Usecase<Output, Input> {
   Future<Mapper<Failure, Output>> call(Input params);
+}
+
+abstract class NoParams extends Equatable {
+  @override
+  List<Object?> get props => [];
 }
