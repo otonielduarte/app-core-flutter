bb67be8c08109fe14628a66758461fbf8a3bd993
Otoniel Moreira Duarte <otonielduarte2@gmail.com>
2021-05-07 13:47:41 -0300

    Update mapper.dart

--- lib/helpers/mapper.dart
+++ lib/helpers/mapper.dart
@@ -1,1 +1,24 @@
-abstract class Mapper<L, R> {}
+import 'package:equatable/equatable.dart';
+
+abstract class Mapper<L, R> extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class Left<L, R> extends Mapper<L, R> {
+  final L _l;
+  const Left(this._l);
+  L get value => _l;
+  
+  @override
+  List<Object?> get props => [_l];
+}
+
+class Right<L, R> extends Mapper<L, R> {
+  final R _r;
+  const Right(this._r);
+  R get value => _r;
+  
+  @override
+  List<Object?> get props => [_r];
+}
