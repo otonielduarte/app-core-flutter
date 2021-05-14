1c7bb4a162f236a036559faf078872d6ee286bea
Otoniel Duarte <otonielduarte2@gmail.com>
2021-05-14 13:41:50 -0300

    create service for navigator

---
+++ lib/app/navigator_service.dart
@@ -0,0 +1,17 @@
+import 'package:core/app/core_utils.dart';
+
+class NavigatorService {
+  Future<dynamic> navigateTo(
+    String routeName, {
+    Map<String, String>? queryParams,
+  }) {
+    if (queryParams != null) {
+      routeName = Uri(path: routeName, queryParameters: queryParams).toString();
+    }
+    return navigatorKey.currentState!.pushNamed(routeName);
+  }
+
+  void goBack() {
+    return navigatorKey.currentState!.pop();
+  }
+}
